34e7a213ed82389ca110cdd8e9b4cdec3aa32576
Max <greermp@gmail.com>
2025-09-21 22:30:36 -0400

    inital sim complete

--- sim.py
+++ sim.py
@@ -3,7 +3,7 @@ from simulation.doe import create_doe
 import math
 
 import numpy as np
-debug = True
+debug = False
 
 def get_sensor_footprint(alt_ft, sensor):
     """
@@ -52,39 +52,57 @@ def mach_to_mps(mach, alt_ft):
     v_ms     = mach * a
     return v_ms
     
-def time_to_fly_xkm(mps, xkm):
+def hours_to_fly_xkm(speed_mps, xkm):
     distance_m = xkm * 1000  
     # time in seconds
-    time_sec = distance_m / mps  
+    time_sec = distance_m / speed_mps  
     # convert to hours
     return time_sec / 3600  
     
-def calc_area_sanitized(mps, endurance, footprint, ingress_dist_km, x=100, y=100):
-    # mps = mach_to_mps(mach, alt_ft)
-    if (debug) : print("Starting endurance: ", endurance )
-    ingress_time = time_to_fly_xkm(mps, ingress_dist_km)
-    endurance -= ingress_time
-    if (debug) : print("After ingress: ", endurance )
+def calc_area_sanitized(speed_mps, endurance, footprint, ingress_dist_km, fuel_reserve_hrs, aoi_width=100, aoi_length=100):
+    if debug : print(f"Starting endurance: {endurance}" )
     
-    x_patrol_time = time_to_fly_xkm(mps, x)
-    if (debug) : print("Patrol time for 1 strip (x): ", x_patrol_time )
-    y_complete = 0
-    while y_complete < 100:
-        if endurance >=  x_patrol_time:
-            endurance -= x_patrol_time
-            # y_complete = 
-    exit
-
+    patrol_area = aoi_width*aoi_length
+    ingress_time = hours_to_fly_xkm(speed_mps, ingress_dist_km)
+    endurance -= ingress_time
+    if debug : print(f"After ingress: {endurance}")
+    endurance -= fuel_reserve_hrs
+    if debug : print(f"After fuel reserve: {endurance}")
+    
+    x_patrol_time = hours_to_fly_xkm(speed_mps, aoi_width)
+    if debug : print(f"Time for 1 strip (aoi_width): {x_patrol_time}" )
+    area_sanitized = 0
+    swath_width = math.sqrt(footprint) # FOV_Width
+    
+    distance_from_base_y = ingress_dist_km + (swath_width/2) # Start in center of swath
+    
+    while area_sanitized < patrol_area:
+        rtb_time = hours_to_fly_xkm(speed_mps, distance_from_base_y)
+        # if debug : print(f"{distance_from_base_y} km from base ({rtb_time}) hrs" )
+        
+        #TODO: Account for moving east/west, and changing distance from base
+        if endurance <  x_patrol_time+rtb_time: # If we have enough TOS to complete this strip
+            print("\nBingo fuel, RTB")
+            if debug: print(f"Endurance remaining: {endurance}. Distance from base {distance_from_base_y}km.  Time for 1 more patrol and RTB:{hours_to_fly_xkm(speed_mps,aoi_width+distance_from_base_y)} ")
+            return area_sanitized
+        
+        
+        endurance                -= x_patrol_time            # deduct "fuel"
+        area_sanitized           += swath_width*aoi_width    # record area sanitized
+        distance_from_base_y     += swath_width              # move aircraft away from base
+        progress_percent          = (area_sanitized / patrol_area) * 100
+        print(f"\rPatrol progress: {progress_percent:.1f}% complete", end="", flush=True)
+    return patrol_area
+            
+            
 def main():
-    ingress_dist_km = 100
+    ingress_dist_km  = 100
+    fuel_reserve_hrs =   0.5
     ### Factors
     # Speed (Mach) from 0.4 to 0.9, e.g., 6 levels
     machs = np.linspace(0.4, 0.9, 6)
     # Altitude from 5k to 25k feet
     altitudes_ft = np.linspace(5000, 25000, 5)
-    
-    # Assume a spacing between sweeps of footprint * (1-sweep_overlap) 
-    sweep_overlap = 0.2
 
     doe = create_doe(machs, altitudes_ft)
     
@@ -102,14 +120,15 @@ def main():
     doe['footprint_km2'] = doe.apply(
         lambda row: get_sensor_footprint(row.alt_ft, row.sensor), axis=1)
 
-    if (False):
-        for idx, row in doe.iterrows():
-            # sanitized = calc_area_sanitized(row["speed_mps"], row["endurance_hr"], row["footprint_km2"], ingress_dist_km)
-            sanitized = calc_area_sanitized(row["speed_mps"], row["endurance_hr"], row["footprint_km2"], ingress_dist_km)
-            doe.loc[idx, "area_sanitized_km2"] = sanitized
-            
-            print(doe.head(1))
-            break
+    for idx, row in doe.iterrows():
+        sanitized = calc_area_sanitized(row["speed_mps"],
+                                        row["endurance_hr"],
+                                        row["footprint_km2"],
+                                        ingress_dist_km, fuel_reserve_hrs)
+        doe.loc[idx, "area_sanitized_km2"] = sanitized
+    
+    doe.to_csv("mpa.csv", columns= [col for col in doe.columns if col != "sensor"] ,index=False)
+
 
 
 
